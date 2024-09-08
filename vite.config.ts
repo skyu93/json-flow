2f2216993f195db2763684c6ad31903ff626a4ab
Teo <teo@clobot.co.kr>
2024-09-08 22:31:08 +0900

    Initial Commit

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+  },
+  plugins: [vue()],
+})
+
